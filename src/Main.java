600ed792ba995e2512afa551db1a8d0b0bcff921
Viet <vietsemantics@gmail.com>
2018-09-16 11:29:00 -0500

    modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        sayGoodBye();
+    }
+
+    public static void sayGoodBye() {
+      System.out.println("GoodBye")
     }
 }
