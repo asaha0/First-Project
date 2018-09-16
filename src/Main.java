89cfd838e8d0bd8dce7a8c797f6b21ebb969f421
Amit Saha <amitsaha@MacBook-Pro-2.local>
2018-09-16 11:50:25 -0500

    _

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         sayGoodBye();
+        sayHi();
     }
 
     public static void sayGoodBye() {
-      System.out.println("GoodBye")
+      System.out.println("GoodBye");
     }
+
+    public static void sayHi() {
+        System.out.println("Hi");
+    }
+
 }
